759c51ebb569c297798461c19fe1953d8b168104
Ahmet <berdiyev707@gmail.com>
2021-07-26 13:32:46 +0300

    modification

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
-import './App.scss';
+import React from 'react'
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import './App.scss';
+import Login from './components/Login';
 
 function App() {
    return (
       <>
          <Router>
             <Switch>
-               <Route exact path="/" />
+               <Route exact path="/" >
+                  <Login />
+               </Route>
             </Switch>
          </Router>
       </>
