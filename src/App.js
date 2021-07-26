48282c492c4c7d67fa0798ead0f66383e6468d40
Ahmet <berdiyev707@gmail.com>
2021-07-26 12:46:58 +0300

    install and create react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import './App.scss';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 function App() {
    return (
       <>
-
+         <Router>
+            <Switch>
+               <Route exact path="/" />
+            </Switch>
+         </Router>
       </>
    );
 }
